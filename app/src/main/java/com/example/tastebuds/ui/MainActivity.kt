35aa8ffcfd3fd1169976334798c0e4746013abc3
Asinsa <asinsa.g@gmail.com>
2021-12-01 20:07:17 +0000

    All fragments open when the right bottom navigation is clicked

--- app/src/main/java/com/example/tastebuds/ui/MainActivity.kt
+++ app/src/main/java/com/example/tastebuds/ui/MainActivity.kt
@@ -4,19 +4,38 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.tastebuds.R
+import com.example.tastebuds.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
 
-        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.bottomNavigation
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+
+        navView.setupWithNavController(navController)
+
 
         /*
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+
         bottomNavigation.setOnNavigationItemSelectedListener { item ->
             when(item.itemId) {
                 R.id.navigation_home -> {
